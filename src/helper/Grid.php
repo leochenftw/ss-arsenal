3a232cc11d68c04da03d0ce80279bedb1bbd72ca
Leo Chen <leo@leochen.co.nz>
2020-05-02 00:30:19 +1200

    adding manyExtraSortable static function

--- src/helper/Grid.php
+++ src/helper/Grid.php
@@ -12,6 +12,16 @@ use SilverStripe\Forms\GridField\GridFieldAddNewButton;
 use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 // use SilverStripe\Forms\GridField\GridFieldArchiveAction;
 // use SilverStripe\Versioned\GridFieldArchiveAction;
+use SilverStripe\Forms\GridField\GridFieldDetailForm;
+use SilverStripe\Forms\GridField\GridFieldConfig;
+use Symbiote\GridFieldExtensions\GridFieldAddExistingSearchButton;
+use Symbiote\GridFieldExtensions\GridFieldEditableColumns;
+use SilverStripe\Forms\GridField\GridFieldButtonRow;
+use SilverStripe\Forms\GridField\GridFieldToolbarHeader;
+use Symbiote\GridFieldExtensions\GridFieldTitleHeader;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
+use SilverStripe\Forms\GridField\GridFieldPageCount;
+use SilverStripe\Forms\GridField\GridFieldPaginator;
 
 class Grid
 {
@@ -60,5 +70,35 @@ class Grid
 
         $grid->setConfig($config);
         return $grid;
+    }
+
+    public static function manyExtraSortable($relation_name, $field_title, $relation_datalist, $class, $fields, $sort_field = 'Sort')
+    {
+        $featuresGridFieldSearchButton = new GridFieldAddExistingSearchButton();
+        $featuresGridFieldSearchButton->setSearchList($class::get());
+
+        // Features field editable columns
+        $featuresGridFieldEditableColumns = new GridFieldEditableColumns();
+        $featuresGridFieldEditableColumns->setDisplayFields($fields);
+
+        // Features field config including base GridFieldConfig_RelationEditor components, custom search button, editable columns and orderable rows
+        $featuresGridFieldConfig = GridFieldConfig::create();
+        $featuresGridFieldConfig->addComponent(new GridFieldButtonRow('before'));
+        $featuresGridFieldConfig->addComponent($featuresGridFieldSearchButton);
+        $featuresGridFieldConfig->addComponent(new GridFieldToolbarHeader());
+        $featuresGridFieldConfig->addComponent(new GridFieldTitleHeader());
+        $featuresGridFieldConfig->addComponent($featuresGridFieldEditableColumns);
+        $featuresGridFieldConfig->addComponent(new GridFieldDeleteAction(true));
+        $featuresGridFieldConfig->addComponent(new GridFieldOrderableRows($sort_field));
+        $featuresGridFieldConfig->addComponent(new GridFieldPageCount('toolbar-header-right'));
+        $featuresGridFieldConfig->addComponent(new GridFieldPaginator());
+        $featuresGridFieldConfig->addComponent(new GridFieldDetailForm());
+
+        return GridField::create(
+            $relation_name,
+            $field_title,
+            $relation_datalist,
+            $featuresGridFieldConfig
+        );
     }
 }
